12fbfc7d9600215fad964c721ce41f4360ed0493
Christopher Fore <contact@chrisfore.us>
2023-01-25 13:34:54 -0500

    Refactor the dev script

--- start_devel_env.sh
+++ start_devel_env.sh
@@ -32,14 +32,13 @@ fi
 
 ANDED=$(( $DOCKER_INSTALLED & $PODMAN_INSTALLED ))
 
-# If both are installed then prompt the user to choose one
-if (( $ANDED == 1 )); then
-    echo -e "Both Docker \033[1;31m(0)\033[0m and Podman \033[1;31m(1)\033[0m seem to be installed, which would you like to use?"
-    echo -n -e "\033[1;31m(0, 1)\033[0m - "
-    read choice
+# Checking the ANDed value of the Docker and Podman variables
+case $ANDED in
+    # If it's 0, that means one of them is installed (or neither)
+    0)
+        if (( $DOCKER_INSTALLED == 1 )); then
+            echo "Detected Docker installation, using that..."
 
-    case $choice in
-        0)
             # Serve the site via Docker and update site when files changed in repo
             docker run --rm \
                 -p 4000:4000 \
@@ -47,8 +46,10 @@ if (( $ANDED == 1 )); then
                 -v "$PWD/.bundle-cache:/usr/local/bundle" \
                 -it "jekyll/jekyll:$JEKYLL_VERSION" \
                 sh -c "jekyll serve --watch"
-            ;;
-        1)
+
+        elif (( $PODMAN_INSTALLED ==1 )); then
+            echo "Detected Podman installation, using that..."
+
             # Serve the site via Podman and update site when files changed in repo
             podman run --rm \
                 -p 4000:4000 \
@@ -56,33 +57,51 @@ if (( $ANDED == 1 )); then
                 -v "$PWD/.bundle-cache:/usr/local/bundle" \
                 -it "docker.io/jekyll/jekyll:$JEKYLL_VERSION" \
                 sh -c "jekyll serve --watch"
-            ;;
-        *)
-            echo "Invalid option selected, please try again. (Or press C-c to exit)..."
-            ;;
-    esac
+        else
+            echo "It appears you don't have Docker or Podman installed. Please ensure it is installed and proceed with running the development environment."
+        fi
+    ;;
+    1)
+        invalid=0
 
-# Executing Docker if Podman isn't installed
-elif (( $DOCKER_INSTALLED == 1 )); then
-    echo "Detected Docker installation, using that..."
-    # Serve the site via Docker and update site when files changed in repo
-    docker run --rm \
-        -p 4000:4000 \
-        -v "$PWD:/srv/jekyll:Z" \
-        -v "$PWD/.bundle-cache:/usr/local/bundle" \
-        -it "jekyll/jekyll:$JEKYLL_VERSION" \
-        sh -c "jekyll serve --watch"
+        while [ $invalid -eq 0 ]; do
+            echo -e "Both Docker \033[1;31m(0)\033[0m and Podman \033[1;31m(1)\033[0m seem to be installed, which would you like to use?"
+            echo -n -e "\033[1;31m(0, 1)\033[0m - "
+            read choice
+            invalid=1
 
-# Executing Podman if Docker isn't installed
-elif (( $PODMAN_INSTALLED == 1 )); then
-    echo "Detected Podman installation, using that..."
-    # Serve the site via Podman and update site when files changed in repo
-    podman run --rm \
-        -p 4000:4000 \
-        -v "$PWD:/srv/jekyll:Z" \
-        -v "$PWD/.bundle-cache:/usr/local/bundle" \
-        -it "docker.io/jekyll/jekyll:$JEKYLL_VERSION" \
-        sh -c "jekyll serve --watch"
-else
-    echo "You don't appear to have Docker or Podman installed."
-fi
+            # Caseing the choice for whether the user wants to use Podman or Docker
+            case $choice in
+                0)
+                    # Serve the site via Docker and update site when files changed in repo
+                    docker run --rm \
+                        -p 4000:4000 \
+                        -v "$PWD:/srv/jekyll:Z" \
+                        -v "$PWD/.bundle-cache:/usr/local/bundle" \
+                        -it "jekyll/jekyll:$JEKYLL_VERSION" \
+                        sh -c "jekyll serve --watch"
+                ;;
+                1)
+                    # Serve the site via Podman and update site when files changed in repo
+                    podman run --rm \
+                        -p 4000:4000 \
+                        -v "$PWD:/srv/jekyll:Z" \
+                        -v "$PWD/.bundle-cache:/usr/local/bundle" \
+                        -it "docker.io/jekyll/jekyll:$JEKYLL_VERSION" \
+                        sh -c "jekyll serve --watch"
+                ;;
+                
+                # If the user entered an invalid option it'll exit
+                *)
+                    echo "Invalid option selected, please try again. (Or press C-c to exit)..."
+                    invalid=0
+                ;;
+            esac
+        done
+    ;;
+    
+    # Default case for if something bad happens, and if something bad happens then something bad happened.
+    *)
+        echo "An Unknown Error occurred, please report this to the maintainers..."
+    ;;
+esac
